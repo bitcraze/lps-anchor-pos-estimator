0a592c348053c18382aaad7ef0ac44e30426b2ee
Aman Sharma <aman.sharmarx@gmail.com>
2016-08-05 11:13:34 +0200

    added numpy functions

--- lpsposest/compactionmatrix.py
+++ lpsposest/compactionmatrix.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 def compactionmatrix(n):
-    mat1 = -ones(n - 1, 1)
-    mat2 = eye(n - 1)
-    cc = concatenate((mat1, mat2), 1)
-    mat3 = zeros(1, n - 1)
-    auxcat = concatenate((1, mat3), 1)
-    dd = concatenate((auxcat, cc))
+    mat1 = -np.ones((n-1, 1))
+    mat2 = np.eye(n-1)
+    cc = np.concatenate((mat1, mat2), 1)
+    mat3 = np.zeros((1, n-1))
+    auxcat = np.concatenate((1, mat3), 1)
+    dd = np.concatenate((auxcat, cc))
 
     return cc, dd
